3183ce069ac894addd4fdd8c563f48a74269c521
Friedrich <business@friedrichrehren.de>
2019-03-12 09:23:18 +0100

    corrected Typo

--- dec2Encryptor/Properties/AssemblyInfo.cs
+++ dec2Encryptor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("dec2Encryptor")]
-[assembly: AssemblyDescription("A multifunctional Encryption Library")]
+[assembly: AssemblyDescription("A multifunktional Encryption Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FriedrichRehren")]
 [assembly: AssemblyProduct("dec2Encryptor")]
