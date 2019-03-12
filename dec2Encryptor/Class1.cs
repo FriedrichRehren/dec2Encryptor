f022ece34d6a82d3791f3fe3a6a1b8b4bc8d6b50
Friedrich <business@friedrichrehren.de>
2019-03-12 09:12:45 +0100

    Add project files.

---
+++ dec2Encryptor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dec2Encryptor
+{
+    public class Class1
+    {
+    }
+}
